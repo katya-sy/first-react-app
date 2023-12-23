6bfdecf4f6c5bf0bf69814352ccb623eb4a92433
katya-sy <katya_sy_28@mail.ru>
2023-12-23 20:21:05 +0900

    create react app, post list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
+import PostList from "./components/postList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JS", desc: "desc desc desc desc" },
+    { id: 2, title: "JS 1", desc: "desc desc desc desc" },
+    { id: 3, title: "JS 2", desc: "desc desc desc desc" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", desc: "desc desc desc desc" },
+    { id: 2, title: "Python 1", desc: "desc desc desc desc" },
+    { id: 3, title: "Python 2", desc: "desc desc desc desc" },
+  ]);
+  const [posts3, setPosts3] = useState([
+    { id: 1, title: "Java", desc: "desc desc desc desc" },
+    { id: 2, title: "Java 2", desc: "desc desc desc desc" },
+    { id: 3, title: "Java 3", desc: "desc desc desc desc" },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostList posts={posts} title="Post about JS" />
+      <PostList posts={posts2} title="Post about Python" />
+      <PostList posts={posts3} title="Post about Java" />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PostItem = (props) => {
+  return (
+    <div className="post">
+      <div className="post__content">
+        <h2>
+          <span>{props.post.id}</span>. {props.post.title}
+        </h2>
+        <div>{props.post.desc}</div>
+      </div>
+      <div className="post__btns">
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
