4958a36e4d0f76c947a17b9a770e8ab0f8c905ab
katya-sy <katya_sy_28@mail.ru>
2023-12-25 16:06:11 +0900

    add hook useFetching

--- src/App.js
+++ src/App.js
@@ -8,24 +8,21 @@ import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePost";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-  const [isPostLoading, setIsPostLoading] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
   }, []);
-
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostLoading(false);
-  }
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -50,6 +47,7 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h2>Error: {postError}</h2>}
       {isPostLoading ? (
         <div
           style={{
