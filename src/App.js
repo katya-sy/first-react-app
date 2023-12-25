0a170b4e462fed23ac1d6483617d42e1653d045e
katya-sy <katya_sy_28@mail.ru>
2023-12-25 11:10:15 +0900

    add sorting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,8 @@ function App() {
     { id: 2, title: "JS 1", desc: "desc desc desc desc" },
     { id: 3, title: "JS 2", desc: "desc desc desc desc" },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState("");
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -22,10 +25,24 @@ function App() {
     );
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      {posts.length !== 0 ? (
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sort"
+        options={[
+          { value: "title", name: "By title" },
+          { value: "desc", name: "By desc" },
+        ]}
+      />
+      {posts.length ? (
         <PostList remove={removePost} posts={posts} title="Post about JS" />
       ) : (
         <h2 style={{ textAlign: "center" }}>No posts</h2>
