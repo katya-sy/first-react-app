2b60a8a9e2dfdcd45521256370831af1a70c2f37
katya-sy <katya_sy_28@mail.ru>
2023-12-26 19:53:07 +0900

    add home page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
+import Home from "../pages/Home";
 import PostIdPage from "../pages/PostIdPage";
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path="/about" element={<About />}></Route>
       <Route path="/posts" element={<Posts />}></Route>
       <Route path="/posts/:id" element={<PostIdPage />}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/error" element={<Error />}></Route>
       <Route path="*" element={<Navigate replace to="/error" />}></Route>
     </Routes>
